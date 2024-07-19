59a4e7ff71713e737e0d69aa367ced61f19e56f4
Dat Tran <viebboy@gmail.com>
2024-07-19 17:19:56 +0300

    add exception catching in init

--- mlproject/__init__.py
+++ mlproject/__init__.py
@@ -58,39 +58,42 @@ else:
     require_checking = True
 
 
-if has_dep and require_checking:
-    with open(STATUS_FILE, "w") as fid:
-        fid.write("")
+try:
+    if has_dep and require_checking:
+        with open(STATUS_FILE, "w") as fid:
+            fid.write("")
 
-    package = "mlproject"
-    branch = "main"
+        package = "mlproject"
+        branch = "main"
 
-    remote_repo = f"https://github.com/viebboy/{package}"
+        remote_repo = f"https://github.com/viebboy/{package}"
 
-    # Get the local path
-    try:
-        src_path = pkg_resources.resource_filename(package, "")
-        is_installed = True
-    except Exception:
-        is_installed = False
+        # Get the local path
+        try:
+            src_path = pkg_resources.resource_filename(package, "")
+            is_installed = True
+        except Exception:
+            is_installed = False
 
-    if is_installed:
-        local_path = os.path.dirname(src_path)
-        local_repo = git.Repo(local_path)
+        if is_installed:
+            local_path = os.path.dirname(src_path)
+            local_repo = git.Repo(local_path)
 
-        # Get the current commit hash of the local repository
-        local_commit = local_repo.head.object.hexsha
+            # Get the current commit hash of the local repository
+            local_commit = local_repo.head.object.hexsha
 
-        # Query the GitHub API for the latest commit on the main branch of the remote repository
-        api_url = f"https://api.github.com/repos/{remote_repo.split('/')[-2]}/{remote_repo.split('/')[-1]}"
-        response = requests.get(f"{api_url}/branches/{branch}")
-        response.raise_for_status()
-        remote_commit = response.json()["commit"]["sha"]
+            # Query the GitHub API for the latest commit on the main branch of the remote repository
+            api_url = f"https://api.github.com/repos/{remote_repo.split('/')[-2]}/{remote_repo.split('/')[-1]}"
+            response = requests.get(f"{api_url}/branches/{branch}")
+            response.raise_for_status()
+            remote_commit = response.json()["commit"]["sha"]
 
-        # Compare the local and remote commit hashes
-        if local_commit != remote_commit:
-            logger.warning(
-                f"local commit diverges from remote commit in package {package}. Please consider updating"
-            )
-            logger.warning(f"local commit on branch {branch}: {local_commit}")
-            logger.warning(f"remote commit on branch {branch}: {remote_commit}")
+            # Compare the local and remote commit hashes
+            if local_commit != remote_commit:
+                logger.warning(
+                    f"local commit diverges from remote commit in package {package}. Please consider updating"
+                )
+                logger.warning(f"local commit on branch {branch}: {local_commit}")
+                logger.warning(f"remote commit on branch {branch}: {remote_commit}")
+except BaseException:
+    pass
