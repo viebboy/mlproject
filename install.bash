caacc8253cbd9aa50733dd5470ecc200eac0be4b
Dat Tran <viebboy@gmail.com>
2024-08-06 17:21:38 +0300

    add install script

--- install.bash
+++ install.bash
@@ -2,7 +2,7 @@
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 pip install -r ${ROOT_DIR}/requirements.txt
-pip install -e .
+pip install -e ${ROOT_DIR}/
 
 # install model-training cli
 pip install -r $ROOT_DIR/cli/model-training/requirements.txt
